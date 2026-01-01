fa9bc9fd40c9036f8a3170d7fa0b8beda7df9c8a
MyGymCompany <Sosiskagodgg@gmail.com>
2026-01-02 01:56:11 +0300

    Тест 1

---
+++ Assets/test/ScrollBarTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.IO;
+public class ScrollBarTest : MonoBehaviour
+{
+    [Header("parametrs")]
+    [Seriliazifiled] int max;
+    void Start()
+    {
+        Debug.Log("start");
+    }
+    
+}
